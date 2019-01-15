7860e19fc58d7755ee1e7c6834b8af2f196cc77e
Chris Chu <chrischu@kkbox.com>
2019-01-15 19:16:39 +0800

    Basic scripts to handle input data and render it

---
+++ script/help.js
@@ -0,0 +1,34 @@
+module.exports = ({ MainOptions, AdvenceOptions, OtherOptions }) => console.log(require('command-line-usage')([
+  {
+    raw: true,
+    content: require('chalk').yellow(require('./banner'))
+  },
+  {
+    header: 'Synopsis',
+    content: [
+      `$ yarn build [...options] {italic <DATA>}`,
+      `$ yarn build [...options] {bold --data} {italic <DATA>}`,
+      `$ yarn build [...options] {bold --input} {italic <FILE_NAME>}`,
+      `$ echo <DATA> | yarn build [...options]`,
+    ],
+  },
+  {
+    header: 'General Options',
+    optionList: MainOptions,
+  },
+  {
+    header: 'Advence Usage',
+    optionList: AdvenceOptions,
+  },
+  {
+    header: 'Others',
+    optionList: OtherOptions,
+  },
+  {
+    header: 'Examples',
+    content: [
+      `$ cat package.json | yarn build {bold --json}`,
+      `$ yarn build {bold --input} {italic package.json} {bold --json}`
+    ],
+  }
+]))
